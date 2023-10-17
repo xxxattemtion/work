ac23597f898fd49612df75d5e70df6a22aa2ab52
Your Name <3164508052@qq.com>
2023-10-17 20:17:57 +0800

    submission

---
+++ hw7.c
@@ -0,0 +1,18 @@
+
+/* Add any includes here */
+#include "hw7.h"
+
+movie g_movie_array[MAX_MOVIES];
+int g_movie_count = 0;
+
+int read_movies(char *filename) {
+    return NOT_IMPLEMENTED_ERR;
+}
+
+void link_movies() {
+
+}
+
+int find_longest_chain(movie *the_movie) {
+    return NOT_IMPLEMENTED_ERR;
+}
