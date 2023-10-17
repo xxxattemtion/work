268f7bc598adf97ceb39d267aa5196ccf5972eb7
mengyi <129582522+xxxattemtion@users.noreply.github.com>
2023-10-17 22:50:57 +0800

    Update hw7.c

--- hw7.c
+++ hw7.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <assert.h>
 
 #define MAX_TITLE_LEN 100
-#define MAX_SEQUELS 10
-#define MAX_BUFFER_LEN (2 * MAX_TITLE_LEN)
+#define MAX_BUFFER_LEN (MAX_TITLE_LEN * 2)
+#define MAX_SEQUELS 5
+#define MAX_MOVIES 50
 
 enum genre {
     ACTION,
-    COMEDY,
-    DRAMA,
-   
+    ADVENTURE,
+    SCI_FI
 };
 
 struct s_movie {
@@ -18,17 +19,16 @@ struct s_movie {
     char sequels[MAX_SEQUELS][MAX_TITLE_LEN];
     enum genre movie_genre;
     int movie_len_minutes;
+    struct s_movie *sequel_ptrs[MAX_SEQUELS];
 };
 
 typedef struct s_movie movie;
 
 #define FILE_ERROR -1
-#define TOO_MUCH_DATA -2
-#define BAD_MOVIE -3
-#define NOT_FOUND -4
+#define BAD_MOVIE -2
+#define TOO_MUCH_DATA -3
 
-// Define your global variables
-movie g_movie[MAX_BUFFER_LEN];
+movie g_movie[MAX_MOVIES];
 int g_movie_count = 0;
 
 int read_movies(char *filename) {
@@ -39,90 +39,114 @@ int read_movies(char *filename) {
         return FILE_ERROR;
     }
 
-    int movie_index = 0;
+    int movie_count = 0;
     char buffer[MAX_BUFFER_LEN];
-
-    while (fgets(buffer, sizeof(buffer), file)) {
-        if (movie_index >= MAX_BUFFER_LEN) {
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        if (movie_count >= MAX_MOVIES) {
             fclose(file);
             return TOO_MUCH_DATA;
         }
 
-        if (sscanf(buffer, "%[^:]:%[^|]|%d", g_movie[movie_index].movie_title,
-            g_movie[movie_index].sequels[0], (int *)&g_movie[movie_index].movie_genre,
-            &g_movie[movie_index].movie_len_minutes) != 4) {
+        if (strlen(buffer) > MAX_BUFFER_LEN) {
             fclose(file);
             return BAD_MOVIE;
         }
 
-        
-        g_movie[movie_index].movie_title[MAX_TITLE_LEN - 1] = '\0';
+        buffer[strcspn(buffer, "\n")] = '\0';
 
-        int sequels_index = 1;
-        char sequel_title[MAX_TITLE_LEN];
-
-        while (sscanf(buffer, "+%[^+]", sequel_title) == 1) {
-            strcpy(g_movie[movie_index].sequels[sequels_index], sequel_title);
-            sequels_index++;
+        strncpy(g_movie[movie_count].movie_title, buffer, MAX_TITLE_LEN);
+        for (int i = 0; i < MAX_SEQUELS; i++) {
+            g_movie[movie_count].sequels[i][0] = '\0';
         }
 
-        
-        for (int i = sequels_index; i < MAX_SEQUELS; i++) {
-            g_movie[movie_index].sequels[i][0] = '\0';
-        }
-
-        movie_index++;
+        g_movie[movie_count].sequel_ptrs[0] = NULL;
+        movie_count++;
     }
 
     fclose(file);
-    g_movie_count = movie_index;
-    return g_movie_count;
+    g_movie_count = movie_count;
+    return movie_count;
 }
 
-int find_sequels(char *title, enum genre genre) {
-    assert(title != NULL);
-
-    int max_duration = 0;
-    int found = 0;
-
+void link_movies() {
     for (int i = 0; i < g_movie_count; i++) {
-        if (strstr(g_movie[i].movie_title, title) != NULL && g_movie[i].movie_genre == genre) {
-            if (g_movie[i].movie_len_minutes > max_duration) {
-                max_duration = g_movie[i].movie_len_minutes;
-                found = 1;
+        for (int j = 0; j < MAX_SEQUELS; j++) {
+            for (int k = 0; k < g_movie_count; k++) {
+                if (strcmp(g_movie[i].sequels[j], g_movie[k].movie_title) == 0) {
+                    g_movie[i].sequel_ptrs[j] = &g_movie[k];
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int find_longest_movie_chain(movie *the_movie) {
+    if (the_movie == NULL) {
+        return 0;
+    }
+
+    int max_minutes = the_movie->movie_len_minutes;
+
+    for (int i = 0; i < MAX_SEQUELS; i++) {
+        if (the_movie->sequel_ptrs[i] != NULL) {
+            int chain_minutes = the_movie->movie_len_minutes + find_longest_movie_chain(the_movie->sequel_ptrs[i]);
+            if (chain_minutes > max_minutes) {
+                max_minutes = chain_minutes;
             }
         }
     }
 
-    if (found) {
-        return max_duration;
-    }
-
-    return NOT_FOUND;
+    return max_minutes;
 }
 
 int main() {
+    int result;
     
-    char filename[] = "movie_list.txt";
-
-    int read_result = read_movies(filename);
-
-    if (read_result < 0) {
-        printf("Error while reading movies: %d\n", read_result);
-        return 1;
+    result = read_movies("bad_movie_MAX_BUFFER_LEN_1.txt");
+    switch (result) {
+        case BAD_MOVIE:
+            printf("Error: BAD_MOVIE\n");
+            break;
+        case TOO_MUCH_DATA:
+            printf("Error: TOO_MUCH_DATA\n");
+            break;
+        default:
+            printf("Read %d movies successfully\n", result);
+            break;
+    }
+    
+    result = read_movies("bad_movie_MAX_MOVIES_1.txt");
+    switch (result) {
+        case BAD_MOVIE:
+            printf("Error: BAD_MOVIE\n");
+            break;
+        case TOO_MUCH_DATA:
+            printf("Error: TOO_MUCH_DATA\n");
+            break;
+        default:
+            printf("Read %d movies successfully\n", result);
+            break;
     }
 
-    
-    char title_prefix[] = "Movie";
-    enum genre genre_to_find = ACTION;
-
-    int longest_sequel_duration = find_sequels(title_prefix, genre_to_find);
-
-    if (longest_sequel_duration == NOT_FOUND) {
-        printf("No sequels found for %s with genre %d.\n", title_prefix, genre_to_find);
-    } else {
-        printf("Longest sequel duration for %s with genre %d: %d minutes\n", title_prefix, genre_to_find, longest_sequel_duration);
+    result = read_movies("good_1.txt");
+    switch (result) {
+        case BAD_MOVIE:
+            printf("Error: BAD_MOVIE\n");
+            break;
+        case TOO_MUCH_DATA:
+            printf("Error: TOO_MUCH_DATA\n");
+            break;
+        default:
+            printf("Read %d movies successfully\n", result);
+            break;
     }
+
+    link_movies();
+
+    int longest_chain = find_longest_movie_chain(&g_movie[0]);
+    printf("\nLongest movie chain: %d minutes\n", longest_chain);
 
     return 0;
 }
+
